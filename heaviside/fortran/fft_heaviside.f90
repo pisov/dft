89d6f39699ea4561ec2fbf0ffdc799a93efab107
Stoyan Pisov <stoyan@mc.phys.uni-sofia.bg>
2018-01-17 16:58:04 +0200

    Update fft_heaviside.f90

--- heaviside/fortran/fft_heaviside.f90
+++ heaviside/fortran/fft_heaviside.f90
@@ -2,24 +2,22 @@ program test
 implicit none
       include "fftw3.f"
 
-      integer n,nf
+      integer n
       parameter (n=128)
 
       integer*8 plan, iplan
 
-      double complex in, out
-      dimension in(N), out(N)
-      double precision :: hi, pi, x
+      double complex inVec, outVec, dimension(N) :: inVec, outVec
       double precision :: timeDomainSum, freqDomainSum
-      double precision :: deltai, fi
+      double precision :: pi, x, delta, fi, t0
 
       integer :: i
 
       pi = 3.141592653589793d0
-!      delta = 1.d0 / n 
+      delta = 1.d0 / (n - 1)
 
-      do i=1,n
-         x = (i-1.d0) / (n - 1)
+      do i = 1, n
+         x = (i-1) * delta
          if (x <= 0.5d0) then
            in(i) = cmplx(1.d0, 0.d0)
          else
@@ -27,23 +25,21 @@ implicit none
          end if
       enddo
 
-
-      call dfftw_plan_dft_1d(plan, n,in,out,fftw_forward, FFTW_ESTIMATE)
-
+      call dfftw_plan_dft_1d(plan, n, inVec, outVec, FFT_FORWARD, FFTW_ESTIMATE)
 
       call dfftw_execute(plan)
 
-      do i=1,n
+      do i = 1, n
          if (i < n/2) then
            fi = i - 1
          else
            fi = i - n - 1
          end if
-         write(*,'(6F15.5)')(i-1.d0) / (n - 1), fi, real(in(i)), real(out(i)), aimag(out(i)), abs(out(i))
+         write(*,'(6F15.5)')(i-1) * delta, fi, real(inVec(i)), real(outVec(i)), aimag(outVec(i)), abs(outVec(i))
       enddo
 
-      write(0,*) 'Parsewal sum time domain: ',sum(abs( in)**2)
-      write(0,*) 'Parsewal sum freq domain: ',sum(abs(out)**2 / n)
+      write(0,*) 'Parsewal sum time domain: ',sum(abs( inVec)**2)
+      write(0,*) 'Parsewal sum freq domain: ',sum(abs(outVec)**2 / n)
 
       call dfftw_destroy_plan(plan)
 
